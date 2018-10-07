5596383a2dcaf374ec17ffcfae5f2efbb25e8b4b
Artem <artmalini@gmail.com>
2018-10-07 21:19:38 +0300

    init

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+<style>
+.wrap {
+	margin-left: 351px;
+	perspective: 646px;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+	width: 100%;
+	border: 1px solid #ddd;
+    transform-style: preserve-3d;
+    transform: rotateX(57deg);
+}
+
+th, td {
+	text-align: left;
+	padding: 48px;
+}
+tr:nth-child(even){background-color: #f2f2f2}
+</style>
+</head>
+</head>
+<body>
+<div style="overflow-x:auto;">
+<div class="wrap">
+	<table>
+		<tbody>
+			<tr>
+			  <th>First Name</th>
+			  <th>Last Name</th>
+			  <th>Points</th>
+			  <th>Points</th>
+			  <th>Points</th>
+			  <th>Points</th>
+			  <th>Points</th>
+			  <th>Points</th>
+			  <th>Points</th>
+			  <th>Points</th>
+			</tr>
+			<tr>
+			  <td>Jill</td>
+			  <td>Smith</td>
+			  <td>50</td>
+			  <td>50</td>
+			  <td>50</td>
+			  <td>50</td>
+			  <td>50</td>
+			  <td>50</td>
+			  <td>50</td>
+			  <td>50</td>
+			</tr>
+			<tr>
+			  <td>Eve</td>
+			  <td>Jackson</td>
+			  <td>94</td>
+			  <td>94</td>
+			  <td>94</td>
+			  <td>94</td>
+			  <td>94</td>
+			  <td>94</td>
+			  <td>94</td>
+			  <td>94</td>
+			</tr>
+			<tr>
+			  <td>Adam</td>
+			  <td>Johnson</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			</tr>
+			<tr>
+			  <td>Adam</td>
+			  <td>Johnson</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			</tr>
+			<tr>
+			  <td>Adam</td>
+			  <td>Johnson</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			</tr>
+			<tr>
+			  <td>Adam</td>
+			  <td>Johnson</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			</tr>
+			<tr>
+			  <td>Adam</td>
+			  <td>Johnson</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			</tr>
+			<tr>
+			  <td>Adam</td>
+			  <td>Johnson</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			</tr>
+			<tr>
+			  <td>Adam</td>
+			  <td>Johnson</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			  <td>67</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+</div>
+</body>
+</html>
